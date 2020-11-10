63b82b04edece526784e9761bd8686211d462bec
yuuki <yuuki323-sld51@kcf.biglobe.ne.jp>
2020-11-10 14:52:49 +0900

    nextjs-blog completed active-rooting

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 
 import Head from 'next/head'
-import { prependOnceListener } from 'process'
 import { getSortedPostsData } from '../lib/posts'
 import Layout, { siteTitle } from './components/layout'
 import utilStyles from './styles/utils.module.css'
+import Link from 'next/Link'
+import Date from './components/date'
 
 export async function getStaticProps(){
   const allPostsData = getSortedPostsData()
@@ -32,11 +33,13 @@ export default function Home({ allPostsData }) {
             <ul className={utilStyles.list}>
                 {allPostsData.map(({ id, date, title }) => (
                   <li className={utilStyles.listItem} key={id}>
-                    {title}
+                    <Link href={`/posts/${id}`}>
+                      <a>{title}</a>
+                    </Link>
                     <br />
-                    {id}
-                    <br />
-                    {date}
+                    <small className={utilStyles.lightText}>
+                        <Date dateString={date} />
+                    </small>
                   </li>
                 ))}                 
             </ul>
